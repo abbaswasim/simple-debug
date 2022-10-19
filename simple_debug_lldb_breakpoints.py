adfdee4f44b30d8464baf1dd6c50acbb42ec2bcd
Wasim Abbas <abbas.wasim@gmail.com>
2022-10-19 10:31:22 +0100

    Only load file if it exists

--- simple_debug_lldb_breakpoints.py
+++ simple_debug_lldb_breakpoints.py
@@ -25,22 +25,21 @@ def load_simple_debug_json(debugger):
                 
     if found_file_name:
         print("simple-debug reading breakpoints from:" + found_file_name)
+        with open(found_file_name, "r") as json_file:
+            data = json.load(json_file)
+            for simple_breakpoint in data:
+                src_file_name = simple_breakpoint["file"]
+                src_breakpoints = simple_breakpoint["breakpoints"]
+                if src_breakpoints:
+                    for breakpoint_locations in src_breakpoints:
+                        if "function" in breakpoint_locations:
+                            func_name = breakpoint_locations["function"]
+                            debugger.HandleCommand('breakpoint set -n ' + func_name)
+                        else:
+                            line_num = breakpoint_locations["line"]
+                            debugger.HandleCommand('breakpoint set -f ' + src_file_name + " -l " + str(line_num))
     else:
         print("Couldn't find .simple-debug.json file in the project or its parents.")
-            
-    with open(found_file_name, "r") as json_file:
-        data = json.load(json_file)
-        for simple_breakpoint in data:
-            src_file_name = simple_breakpoint["file"]
-            src_breakpoints = simple_breakpoint["breakpoints"]
-            if src_breakpoints:
-                for breakpoint_locations in src_breakpoints:
-                    if "function" in breakpoint_locations:
-                        func_name = breakpoint_locations["function"]
-                        debugger.HandleCommand('breakpoint set -n ' + func_name)
-                    else:
-                        line_num = breakpoint_locations["line"]
-                        debugger.HandleCommand('breakpoint set -f ' + src_file_name + " -l " + str(line_num))
 
 # And the initialization code to add your commands 
 def __lldb_init_module(debugger, dict):
